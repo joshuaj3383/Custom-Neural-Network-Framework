26cfa8cd23ef22da94c3187b679844bd8a8040e7
joshuaj3383 <mcraftj805@gmail.com>
2026-02-03 22:01:03 +0800

    clean up + gitignore

--- tests/linear.py
+++ tests/linear.py
@@ -2,17 +2,18 @@ from simple_nn.NeuralNetwork import NeuralNetwork as nn
 from simple_nn.Layer import Layer
 from simple_nn.ActivationFunction import ActivationFunction
 
-layer = Layer([1,1],1,ActivationFunction.NONE)
+if __name__ == "__main__":
+    layer = Layer([1,1],1,ActivationFunction.NONE)
 
-nn = nn([layer])
+    nn = nn([layer])
 
-print("Generating 5000 samples between (-100,100) for 50x-12.2 with noise_std=2.5")
-X, Y = nn.generate_data(5000, (-100,100), lambda x: 50*x-12.2, 2.5)
+    print("Generating 5000 samples between (-100,100) for 50x-12.2 with noise_std=2.5")
+    X, Y = nn.generate_data(5000, (-100,100), lambda x: 50*x-12.2, 2.5)
 
-print("\nTraining============================")
-nn.train(X, Y, 10, 1e-4, .9)
+    print("\nTraining============================")
+    nn.train(X, Y, 10, 1e-4, .9)
 
-print("\nExpected=============================")
-print("50.00x + -12.20")
-print("\nNeural Networks Prediction===========")
-print(f"{round(nn.layers[0].weights[0][0],2)}x + {round(nn.layers[0].biases[0][0],2)}")
+    print("\nExpected=============================")
+    print("50.00x + -12.20")
+    print("\nNeural Networks Prediction===========")
+    print(f"{round(nn.layers[0].weights[0][0],2)}x + {round(nn.layers[0].biases[0][0],2)}")
--- tests/non_linear.py
+++ tests/non_linear.py
@@ -3,90 +3,91 @@ from simple_nn.Layer import Layer
 from simple_nn.ActivationFunction import ActivationFunction
 import matplotlib.pyplot as plt
 
-print("Info===============================")
-print("Creating Neural Network for layers of size [1,25][25,25][25,1]")
-print("This corresponds to three layers with 1 input, 25 hidden neurons, 25 hidden neurons, one output layer")
-print("The hidden neurons are leak_relu to remove linearity and to avoid dead neurons")
-print("The output layer have no activation function to allow for negative values")
+if __name__ == "__main__":
+    print("Info===============================")
+    print("Creating Neural Network for layers of size [1,25][25,25][25,1]")
+    print("This corresponds to three layers with 1 input, 25 hidden neurons, 25 hidden neurons, one output layer")
+    print("The hidden neurons are leak_relu to remove linearity and to avoid dead neurons")
+    print("The output layer have no activation function to allow for negative values")
 
-#Create Layers
-layer0 = Layer([1,25],-1,ActivationFunction.LEAKY_RELU)
-layer1 = Layer([25,25],.5,ActivationFunction.LEAKY_RELU)
-layer2 = Layer([25,1],1,ActivationFunction.NONE)
-layers = [layer0, layer1, layer2]
-#Create NN
-nn = NeuralNetwork(layers)
+    #Create Layers
+    layer0 = Layer([1,25],-1,ActivationFunction.LEAKY_RELU)
+    layer1 = Layer([25,25],.5,ActivationFunction.LEAKY_RELU)
+    layer2 = Layer([25,1],1,ActivationFunction.NONE)
+    layers = [layer0, layer1, layer2]
+    #Create NN
+    nn = NeuralNetwork(layers)
 
-#Train
-print("=====================================")
-print("Generating 1000 samples for x in -7,5 with .25x^3+1.5x^2-3 with noist_std=2")
-print("These numbers were chosen to provide an interesting graph with 3 roots and 2 minima/maxima")
-X, Y = nn.generate_data(5000, (-7,5), lambda x:.25*x*x*x+1.5*x*x-3, 2.5)
-print("\nTraining=============================")
-nn.train(X, Y, 7, .0003, .9)
+    #Train
+    print("=====================================")
+    print("Generating 1000 samples for x in -7,5 with .25x^3+1.5x^2-3 with noist_std=2")
+    print("These numbers were chosen to provide an interesting graph with 3 roots and 2 minima/maxima")
+    X, Y = nn.generate_data(5000, (-7,5), lambda x:.25*x*x*x+1.5*x*x-3, 2.5)
+    print("\nTraining=============================")
+    nn.train(X, Y, 7, .0003, .9)
 
-#Test
-x = -7
-in_nn = []
-out_nn = []
-while x <= 5:
-    in_nn.append(round(x,3))
-    out_nn.append(round(nn.predict([x]).tolist()[0][0],3))
-    x += 0.05
+    #Test
+    x = -7
+    in_nn = []
+    out_nn = []
+    while x <= 5:
+        in_nn.append(round(x,3))
+        out_nn.append(round(nn.predict([x]).tolist()[0][0],3))
+        x += 0.05
 
-#Plot
-print("\nPlotting===================================")
-plt.figure()
-plt.scatter(X,Y,color="red",s=.5)
-plt.title("Data")
-plt.scatter(in_nn,out_nn,color="blue",s=10)
-plt.title("Data vs Predictions")
-plt.xlabel("Input")
-plt.ylabel("Output")
-plt.show()
-print("Done")
+    #Plot
+    print("\nPlotting===================================")
+    plt.figure()
+    plt.scatter(X,Y,color="red",s=.5)
+    plt.title("Data")
+    plt.scatter(in_nn,out_nn,color="blue",s=10)
+    plt.title("Data vs Predictions")
+    plt.xlabel("Input")
+    plt.ylabel("Output")
+    plt.show()
+    print("Done")
 
-#Check zeros, minima, and maxima
-zeros = []
-minima = []
-maxima = []
-#Loops through inputs, avoid first and last
-i = 1
-while i < len(in_nn) - 1:
-    prv = out_nn[i-1]
-    cur = out_nn[i]
-    nxt = out_nn[i+1]
+    #Check zeros, minima, and maxima
+    zeros = []
+    minima = []
+    maxima = []
+    #Loops through inputs, avoid first and last
+    i = 1
+    while i < len(in_nn) - 1:
+        prv = out_nn[i-1]
+        cur = out_nn[i]
+        nxt = out_nn[i+1]
 
-    x = in_nn[i]
+        x = in_nn[i]
 
-    # If there is a change in sign it is a root
-    if prv < 0 < nxt or prv > 0 > nxt:
-        zeros.append((x,cur))
-        i += 1 #avoid doubling roots
-
-
-    # If cur is less than both its neighbors it is a minima
-    if prv > cur and nxt > cur:
-        minima.append((x, cur))
-
-    # If cur is greater than both its neighbors it is a maxima
-    if prv < cur and nxt < cur:
-        maxima.append((x,cur))
-
-    i+=1
+        # If there is a change in sign it is a root
+        if prv < 0 < nxt or prv > 0 > nxt:
+            zeros.append((x,cur))
+            i += 1 #avoid doubling roots
 
 
+        # If cur is less than both its neighbors it is a minima
+        if prv > cur and nxt > cur:
+            minima.append((x, cur))
 
-print("\nAnalysis==========================================================")
-print(f"Expected zeros:   [(-5.62,0), (-1.66,0), (+1.28,0)]")
-print(f"Predicted Zeros:  {zeros}")
-print(f"Expected minima:  [(0,-3)]")
-print(f"Predicted minima: {minima}")
-print(f"Expected maxima:  (-4,5)")
-print(f"Predicted maxima: {maxima}")
+        # If cur is greater than both its neighbors it is a maxima
+        if prv < cur and nxt < cur:
+            maxima.append((x,cur))
+
+        i+=1
 
 
-#print([(x,y) for x,y in zip(in_nn, out_nn)])
+
+    print("\nAnalysis==========================================================")
+    print(f"Expected zeros:   [(-5.62,0), (-1.66,0), (+1.28,0)]")
+    print(f"Predicted Zeros:  {zeros}")
+    print(f"Expected minima:  [(0,-3)]")
+    print(f"Predicted minima: {minima}")
+    print(f"Expected maxima:  (-4,5)")
+    print(f"Predicted maxima: {maxima}")
+
+
+    #print([(x,y) for x,y in zip(in_nn, out_nn)])
 
 
 
