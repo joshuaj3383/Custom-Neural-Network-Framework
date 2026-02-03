iris + softmax fix

simple_nn/Layer.py
@@ -56,18 +56,7 @@ class Layer:
             inputs = np.array(inputs).reshape(1, -1)
 
         z = inputs @ self.weights + self.biases
-        return self.apply_activation(z)
-
-    def apply_activation(self, z: np.ndarray) -> np.ndarray:
-        if self.activationFunction == ActivationFunction.RELU:
-            return np.maximum(0, z)
-        elif self.activationFunction == ActivationFunction.LEAKY_RELU:
-            return np.maximum(0.01 * z, z)
-        elif self.activationFunction == ActivationFunction.SIGMOID:
-            return 1 / (1 + np.exp(-z))
-        elif self.activationFunction == ActivationFunction.TANH:
-            return np.tanh(z)
-        return z  # ActivationFunction.NONE
+        return self.activationFunction.calcActivationFunction(z)
 
     def changeWeights(self, delta_weight: np.ndarray) -> None:
         if delta_weight.shape != self.weights.shape:

simple_nn/NeuralNetwork.py
@@ -60,16 +60,28 @@ class NeuralNetwork:
 
     def backprop(self, X: np.ndarray, Y: np.ndarray, lr: float):
         """Perform one backpropagation step"""
+
+        # Make sure that the data is at least 2d for dimsions sake
+        X = np.atleast_2d(X)
+        Y = np.atleast_2d(Y)
+
+        # Setup
         activations = self.forward(X)
         error = [None] * len(self.layers)
 
         # Output layer error
         output = activations[-1]
 
-        #We force softmax to be paired with CEL for practicality and also math
+        # Make sure shapes line up bc of brocasting
+        # eg. (1,3) - (1,1) will fly by (thx iris)
+        if output.shape != Y.shape:
+            raise ValueError(f"Network output shape: {output.shape} != data shape: {Y.shape}")
+
+        # We force softmax to be paired with CEL for practicality and also math
         if self.layers[-1].activationFunction == ActivationFunction.SOFTMAX:
             error[-1] = output - Y
         else:
+            #print(f"sizes: ({output.shape}-{Y.shape}) * {self.layers[-1].activationFunction.calcDerivative(output).shape}")
             error[-1] = (output - Y) * self.layers[-1].activationFunction.calcDerivative(output)
         # Backpropagate through hidden layers
         for i in reversed(range(len(self.layers) - 1)):
@@ -94,11 +106,10 @@ class NeuralNetwork:
         for epoch in range(epochs):
             net_loss = 0
             for x, y in zip(X, Y):
-                x = np.atleast_1d(x)
-                y = np.atleast_1d(y)
-
+                #print(x,y)
                 self.backprop(x, y, lr)
                 loss = self.loss_func(self.forward(x)[-1], y)
+                #print(self.forward(x)[-1],y,loss)
                 net_loss += loss
 
             avg_loss = net_loss / len(X)
@@ -111,8 +122,8 @@ class NeuralNetwork:
 
         for x, y in zip(X, Y):
             # Force dimensions for 1d
-            x = np.atleast_1d(x)
-            y = np.atleast_1d(y)
+            x = np.atleast_2d(x)
+            y = np.atleast_2d(y)
 
             pred = self.predict(x)
             predictions.append(pred)
@@ -132,8 +143,8 @@ class NeuralNetwork:
     @staticmethod
     def cel(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
         """Cross entropy loss"""
-        # Log of predicted probabilities, avoid log(0) with a small epsilon
-        epsilon = 1e-7
+        epsilon = 1e-5
+        #print(Y_pred)
         return float(-1 * np.sum(Y_true * np.log(Y_pred + epsilon)))  # Cross-entropy
 
     @staticmethod

tests/iris.py
@@ -1,25 +1,50 @@
+import numpy as np
 from sklearn import datasets
+from sklearn.utils import shuffle
 
 from simple_nn.NeuralNetwork import NeuralNetwork
 from simple_nn.Layer import Layer
 from simple_nn.ActivationFunction import ActivationFunction
-import matplotlib.pyplot as plt
+
+
+def one_hot(x: int, amount: int) -> np.ndarray:
+    l = [0] * amount
+    l[x] = 1
+    return np.array(l)
 
 iris = datasets.load_iris()
 
 X = iris.data
 Y = iris.target
 
-layer0 = Layer([4,5],-1,ActivationFunction.LEAKY_RELU)
-layer1 = Layer([5,5],-.5,ActivationFunction.LEAKY_RELU)
-layer2 = Layer([5,3],.5,ActivationFunction.SOFTMAX)
+Y = [one_hot(y, 3) for y in Y]
 
-
-layers = [layer0, layer1, layer2]
-
+layer0 = Layer([4,10],1,ActivationFunction.LEAKY_RELU)
+layer1 = Layer([10,10],1,ActivationFunction.LEAKY_RELU)
+layer2 = Layer([10,3],1,ActivationFunction.SOFTMAX)
+layers = [layer0, layer1,layer2]
 nn = NeuralNetwork(layers)
 
-nn.train(X,Y,5,0.01)
+# To remove order
+X, Y = shuffle(X, Y, random_state=1)
+nn.train(X,Y,10,.01,.9)
+# Again with different order
+X, Y = shuffle(X, Y, random_state=55)
+nn.train(X,Y,10,.0005,.7)
+
+pred, _ = nn.test(X,Y)
+
+c = 0
+for y, p in zip(Y, pred):
+    predicted_class = np.argmax(p)
+    true_class = np.argmax(y)
+
+    if predicted_class == true_class:
+        c += 1
+
+# Calculate accuracy
+accuracy = c / len(Y) * 100
+print(f"\nAccuracy: {c}/{len(Y)} = {round(accuracy, 3)}%")
 
 
 